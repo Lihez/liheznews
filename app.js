62131f4903bce316b4b9fd85a230d4e7ee3479f5
Yolster <yolster@protonmail.com>
2021-12-14 13:07:08 +0300

    Cod işte

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
+const session = require('express-session');
+var settings = require('./settings.json')
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : settings.sql.host,
+  user     : settings.sql.user,
+  password : settings.sql.password,
+  database : settings.sql.db
+});
+
+connection.connect((err)=> {
+  if (err){
+      throw err;
+  }
+  console.log('MySQL veritabanına başarıyla bağlanıldı.'); 
+});
+
+module.exports = {session: session, con:connection };
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -18,11 +34,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'xAfeA23rop3mer3onrua3ebgrj3nr42kj3',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -31,7 +50,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'product' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var {session,con} = require('../app')
+
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async(req, res) => {
+  
+  const veri = await new Promise((resolve, reject) => {
+    con.query(`SELECT * FROM news ORDER BY id DESC LIMIT 12`, function (err, result) {
+        if (err)
+            reject(err);
+        resolve(result);
+    });
+});
+
+const spor = await new Promise((resolve, reject) => {
+  con.query(`SELECT * FROM news WHERE category='spor' ORDER BY id DESC LIMIT 12`, function (err, result) {
+      if (err)
+          reject(err);
+      resolve(result);
+  });
+});
+
+const ekonomi = await new Promise((resolve, reject) => {
+  con.query(`SELECT * FROM news WHERE category='ekonomi' ORDER BY id DESC LIMIT 12`, function (err, result) {
+      if (err)
+          reject(err);
+      resolve(result);
+  });
+});
+
+  res.render('index', { 
+    title: 'Home - Lehiz News',
+    data: veri,
+    spor:spor,
+    ekonomi:ekonomi
+  });
 });
 
 module.exports = router;
